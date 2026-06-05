559f603f828344a42db2613ffc667894ed007c84
rhuanantunes2007-design <brunodemoraeswkk@gmail.com>
2026-06-05 08:55:29 -0300

    Firuliru pulapula

--- Assets/Script/ControladorPersonagem.cs
+++ Assets/Script/ControladorPersonagem.cs
@@ -6,6 +6,8 @@ public class ControladorPersonagem : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     public float vel;   
+    public float jump;
+    public GameObject GroundCheck;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +20,14 @@ public class ControladorPersonagem : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        if (rb2d.velocity.magnitude < 5){
-        rb2d.velocity += new Vector2(vel,0)*horizontalInput*Time.deltaTime;
+        if (rb2d.velocity.magnitude < 5) {
+            rb2d.velocity += new Vector2(vel,0)*horizontalInput*Time.deltaTime;
         }
+
+        if (Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.Space)) {
+            rb2d.velocity = new Vector2(rb2d.velocity.x,jump);
+        }
+
+        
     }
 }
